4f37760913098ca80845507efcc067f8067cc82a
JoanMonroy05 <joanunimonroy05@gmail.com>
2025-06-07 08:13:34 -0500

    Agregando la funcionalidadd de agregar un nuevo producto

--- js/productsService.js
+++ js/productsService.js
@@ -29,6 +29,7 @@ function fetchProducts() {
 }
 function mostrarProductos(products) {
     let listProducts = `
+        <button onclick="addProduct()" type="button" class="btn btn-outline-success"><i class="fa-solid fa-box"></i> +</button>
         <table class="table">
             <thead>
                 <tr>
@@ -114,4 +115,101 @@ function showModal(product) {
     modal.show();
 }
 
+function addProduct() {
+    const modalProductAdd = `
+        <div class="modal fade" id="showModalProductAdd" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-md">
+            <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="exampleModalLabel">Ver Usuario</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <div class="card">
+                    <div class="card-body">
+                    <form id="formAddProduct">
+                        <div class="row">
+                            <div class="col">
+                                <input type="text" id="titulo" class="form-control" placeholder="Titulo del Producto" aria-label="First name" required>
+                            </div>
+                        </div>
+                        <div class="row mt-3">
+                            <div class="col">
+                                <label for="descripcion" class="form-label">Descripción</label>
+                                <textarea class="form-control" id="descripcion" rows="3"></textarea>
+                            </div>
+                        </div>
+                        <div class="row mt-3">
+                            <div class="col">
+                                <input type="number" id="precio" class="form-control" placeholder="Precio" aria-label="First name" required>
+                            </div>
+                            <div class="col">
+                                <input type="number" id="cantidad" class="form-control" placeholder="Cantidad" aria-label="First name" required>
+                            </div>                            
+                        </div>
+                        <div class="row mt-3">
+                            <div class="col">
+                                <input type="url" id="image" class="form-control" placeholder="Link del producto" aria-label="Last name" required>
+                            </div>
+                        </div>
+                        <div class="row mt-3 ">
+                            <div class="col text-center">
+                                <button onclick="saveProduct()" type="button" class="btn btn-success"><i class="fa-solid fa-floppy-disk"></i> Guardar</button>
+                            </div>
+                        </div> 
+                    </form>
+                </div>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            </div>
+            </div>
+        </div>
+        </div>
+    `;
+    document.querySelector('#showModal').innerHTML = modalProductAdd;
+    const modal = new bootstrap.Modal(document.getElementById('showModalProductAdd'));
+    modal.show();
+}
+
+function saveProduct() {
+    const form = document.querySelector('#formAddProduct');
+    if (form.checkValidity()) {
+        const titulo = document.querySelector('#titulo').value;
+        const descripcion = document.querySelector('#descripcion').value;
+        const precio = document.querySelector('#precio').value;
+        const cantidad = document.querySelector('#cantidad').value;
+        const imagen = document.querySelector('#image').value;
+        const product = {titulo, descripcion, precio, cantidad, imagen};
+
+        const URL = 'https://dummyjson.com/products/add';
+        fetch(URL, {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json',
+                'x-api-key': 'reqres-free-v1'
+            },
+            body: JSON.stringify(product)
+        })
+        .then((response) =>response.json().then(data => ({status: response.status, info: data})))
+        .then(result => {
+            if (result.status === 201) {
+                document.querySelector("#contenido-dinamico").innerHTML = '';
+                alertBuilder('success', 'Producto creado correctamente.');
+            } else {
+                document.querySelector("#contenido-dinamico").innerHTML = '';
+                alertBuilder('danger', 'El producto no se agrego a la API.');
+            }
+            const modalId = document.getElementById('showModalProductAdd');
+            const modal = bootstrap.Modal.getInstance(modalId);
+            modal.hide();
+        })
+    } else {
+        form.reportValidity();
+    }
+}
+
 window.getProducts = getProducts;
+window.addProduct = addProduct;
+window.saveProduct = saveProduct;
